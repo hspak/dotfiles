d40f270da9891a743fc6b53d4719ed24047b3dcf
Hong Shick Pak <hong@hspak.com>
2014-09-08 21:27:58 -0400

    ycm

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -37,9 +37,7 @@ import ycm_core
 flags = [
 '-Wall',
 '-Wextra',
-'-Werror',
-'-Wc++98-compat',
-'-Wno-long-long',
+# '-Werror',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
@@ -51,22 +49,18 @@ flags = [
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c',
-'-isystem',
-'../BoostParts',
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I',
-'.',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'/usr/include',
-'-isystem',
-'/usr/local/include',
+'-x', 'c',
+'-isystem', '../BoostParts',
+'-isystem', '../llvm/include',
+'-isystem', '../llvm/tools/clang/include',
+'-I', '.',
+'-I', './ClangCompleter',
+'-isystem', '/usr/include',
+'-isystem', '/usr/include/sys',
+'-isystem', '/usr/local/include',
+
+# Going to have to check this every update
+'-isystem', '/usr/lib/gcc/x86_64-unknown-linux-gnu/4.9.1/include',
 ]
 
 
