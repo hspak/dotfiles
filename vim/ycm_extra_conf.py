67ab0c1281518535bb00543e39bc4fcc8fb4eb89
Hong Shick Pak <hong@hspak.com>
2015-01-09 15:51:54 -0500

    vim

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -50,6 +50,7 @@ flags = [
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x', 'c',
+'-D', 'DEBUG',
 '-isystem', '../BoostParts',
 '-isystem', '../llvm/include',
 '-isystem', '../llvm/tools/clang/include',
