27cbae68c4e89fc95283df4b66d71c7595bc9f9c
Hong Shick Pak <hong@hspak.com>
2024-05-17 22:28:04 -0700

    stuff

--- arch/bin/aur.sh
+++ arch/bin/aur.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -e
+
 pkgbase=$1
 git clone "ssh://aur@aur.archlinux.org/${pkgbase}.git"
 git remote add aur "ssh://aur@aur.archlinux.org/${pkgbase}.git"
+cd "${pkgbase}"
 git fetch aur
 makepkg
 makepkg --printsrcinfo > .SRCINFO
